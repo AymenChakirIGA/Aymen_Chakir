77c420add2628b3975abc4acbdd396a32cc8ed38
anasszaoui <zaouiazaziop@gmail.com>
2024-04-04 13:23:37 +0000

    Ajoutez des fichiers projet.

---
+++ Aymen_Chakir/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Aymen_Chakir
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Aymen_Chakir/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aymen_Chakir
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtb1.Text = slid1.Value.ToString("0");
+        }
+
+        private void slid2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtb2.Text = slid2.Value.ToString("0");
+        }
+
+        private void slid3_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtb3.Text = slid3.Value.ToString("0");
+        }
+
+        private void but_Click(object sender, RoutedEventArgs e)
+        {
+            txtb11.Text = txtb1.Text;
+            txtb22.Text = txtb2.Text;
+            txtb33.Text = txtb3.Text;
+            Listb.Items.Add(txtb2.Text);
+            Listb.Items.Add(txtb3.Text);
+            Listb.Items.Add(txtb1.Text);
+            txtb6.Text = Listb.Items[0].ToString();
+            txtb5.Text = Listb.Items[1].ToString();
+            txtb4.Text = Listb.Items[2].ToString();
+        }
+        
+    }
+}
